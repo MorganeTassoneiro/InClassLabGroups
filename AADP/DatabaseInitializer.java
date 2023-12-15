50b6dc9a8ed6e680ea2fae374a9f39bd756dbdfe
MorganeTassoneiro <2021327@student.cct.ie>
2023-12-15 13:59:28 +0000

    Initializer

---
+++ AADP/DatabaseInitializer.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author morga
+ */
+public class DatabaseInitializer {
+   public static void main(String[] args, Iterable<String> teams) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    for (String team : teams) {
+                stmt.execute(
+                        "CREATE TABLE IF NOT EXISTS "+ team + " ("
+                                + "name VARCHAR(30) NOT NULL,"
+                                + "number INT NOT NULL PRIMARY KEY,"
+                                + "birth VARCHAR(30),"
+                                + "position VARCHAR(30),"
+                                + "goalsScored INT,"
+                                + "background TEXT(1000));"
+                );                
+            }
+}
+
+private static void insertPlayerData(Connection conn, String teamName, String name, int number, String birth, String position, int goalsScored, String background) throws SQLException {
+    Statement stmt = conn.createStatement();
+    stmt.execute(String.format("INSERT INTO %s (name, number, birth, position, goalsScored, background) "
+                    + "VALUES (\"%s\", %d, \"%s\", \"%s\", %d,  \"%s\") ;",
+            teamName, name, number, birth, position, goalsScored, background)
+    
+    );
+}}
+
+
+  
